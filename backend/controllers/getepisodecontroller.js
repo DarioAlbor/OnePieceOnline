ace486e23aada83f3760d47ffb0186e1ce2fa61e
DarioAlbor <darioalbor03@gmail.com>
2024-07-20 17:41:52 -0300

    new readme.md

--- backend/controllers/getepisodecontroller.js
+++ backend/controllers/getepisodecontroller.js
@@ -1,79 +1,66 @@
 const puppeteer = require('puppeteer');
-require("dotenv").config();
 
 const customWait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const getEpisodes = async (req, res) => {
-  const { sortOrder } = req.query;
-  try {
-    const browser = await puppeteer.launch({
-      args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-      ],
-      executablePath:
-        process.env.NODE_ENV === "production"
-          ? process.env.PUPPETEER_EXECUTABLE_PATH
-          : puppeteer.executablePath(),
-    });
+    const { sortOrder } = req.query;
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www3.animeflv.net/anime/one-piece-tv', { waitUntil: 'networkidle2' });
 
-    const page = await browser.newPage();
-    await page.goto('https://www3.animeflv.net/anime/one-piece-tv', { waitUntil: 'networkidle2' });
+        const episodes = await page.evaluate(() => {
+            const episodeList = [];
+            const episodeElements = document.querySelectorAll('ul.ListCaps li a');
+            episodeElements.forEach(el => {
+                const titleElement = el.querySelector('h3.Title');
+                const episodeElement = el.querySelector('p');
+                const imgElement = el.querySelector('img');
 
-    const episodes = await page.evaluate(() => {
-      const episodeList = [];
-      const episodeElements = document.querySelectorAll('ul.ListCaps li a');
-      episodeElements.forEach(el => {
-        const titleElement = el.querySelector('h3.Title');
-        const episodeElement = el.querySelector('p');
-        const imgElement = el.querySelector('img');
+                if (titleElement && episodeElement && imgElement) {
+                    const episode = {
+                        title: titleElement.innerText,
+                        episode: episodeElement.innerText,
+                        link: el.href,
+                        thumbnail: imgElement.getAttribute('data-src')
+                    };
+                    episodeList.push(episode);
+                }
+            });
+            return episodeList;
+        });
 
-        if (titleElement && episodeElement && imgElement) {
-          const episode = {
-            title: titleElement.innerText,
-            episode: episodeElement.innerText,
-            link: el.href,
-            thumbnail: imgElement.getAttribute('data-src')
-          };
-          episodeList.push(episode);
+        const highestEpisode = episodes.reduce((max, episode) => {
+            const num = parseInt(episode.episode.replace(/\D/g, ''));
+            return num > max ? num : max;
+        }, 0);
+
+        const existingEpisodes = new Set(episodes.map(ep => parseInt(ep.episode.replace(/\D/g, ''))));
+
+        for (let i = highestEpisode - 1; i > 0; i--) {
+            if (!existingEpisodes.has(i)) {
+                const episode = {
+                    title: "One Piece",
+                    episode: `Episodio ${i}`,
+                    link: `https://www3.animeflv.net/ver/one-piece-tv-${i}`,
+                    thumbnail: `https://cdn.animeflv.net/screenshots/7/${i}/th_3.jpg`
+                };
+                episodes.push(episode);
+            }
         }
-      });
-      return episodeList;
-    });
 
-    const highestEpisode = episodes.reduce((max, episode) => {
-      const num = parseInt(episode.episode.replace(/\D/g, ''));
-      return num > max ? num : max;
-    }, 0);
+        episodes.sort((a, b) => {
+            const numA = parseInt(a.episode.replace(/\D/g, ''));
+            const numB = parseInt(b.episode.replace(/\D/g, ''));
+            return sortOrder === 'asc' ? numA - numB : numB - numA;
+        });
 
-    const existingEpisodes = new Set(episodes.map(ep => parseInt(ep.episode.replace(/\D/g, ''))));
-
-    for (let i = highestEpisode - 1; i > 0; i--) {
-      if (!existingEpisodes.has(i)) {
-        const episode = {
-          title: "One Piece",
-          episode: `Episodio ${i}`,
-          link: `https://www3.animeflv.net/ver/one-piece-tv-${i}`,
-          thumbnail: `https://cdn.animeflv.net/screenshots/7/${i}/th_3.jpg`
-        };
-        episodes.push(episode);
-      }
+        await browser.close();
+        res.json(episodes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching episodes');
     }
-
-    episodes.sort((a, b) => {
-      const numA = parseInt(a.episode.replace(/\D/g, ''));
-      const numB = parseInt(b.episode.replace(/\D/g, ''));
-      return sortOrder === 'asc' ? numA - numB : numB - numA;
-    });
-
-    await browser.close();
-    res.json(episodes);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error fetching episodes');
-  }
 };
 
 module.exports = getEpisodes;
--- readme.md
+++ readme.md
@@ -1,1 +1,70 @@
-proximamente..
+# 🔴 Challenge Pokemon
+
+[Español](readme.md) <img src="https://flagicons.lipis.dev/flags/4x3/ar.svg" width="20"/> / [Inglés](readme_en.md) <img src="https://flagicons.lipis.dev/flags/4x3/us.svg" width="20"/>
+
+## 📖 Sobre el proyecto
+
+Este proyecto es un **Web Scraping** a los servicios de *AnimeFLV* para obtener todos los capitulos de One Piece y posteriormente su etiqueta IFrame dependiendo el reproductor que se seleccione.
+
+## 🧠 Algoritmo
+
+- Lista todos los capitulos tomando el más reciente desde la pagina de AnimeFLV.
+- Los capitulos que no llega a obtener en el primer scraping (Debido a que la web los genera a medida que se scrollea) los genera en la respuesta JSON manualmente el controlador `getepisodecontroller.js` siguiendo la logica de los anteriores.
+- Cuando se selecciona un controlador se ingresa al link del mismo y se obtiene el IFrame principal.
+- En caso de que el reproductor principal no funcione, se pueden obtener los demás mediante Scraping a las etiquetas de opciones para así obtener el IFrame según la opción seleccionada.
+- Se implementaron botones de busqueda, paginación y ordenamiento para mayor organización de los capitulos. También esto evita que se sobrecargue la pagina.
+
+## 💻 Tecnologías usadas
+
+Backend:
+- **Framework:** Node.js
+- **Scraping:** Puppeter
+- **Gestor de paquetes:** NPM
+
+Frontend:
+- **Librería:** React
+- **Componentes UI:** ChakraUI
+- **Gestor de paquetes:** NPM
+
+## 🚀 ¿Cómo deployarlo?
+
+### Clonar el repositorio
+
+1. Clona el repositorio desde GitHub: `git clone https://github.com/DarioAlbor/OnePieceOnline.git`
+2. Navega al directorio del proyecto: `cd OnePieceOnline`
+
+### Configurar y arrancar el servidor
+
+1. Navega al directorio del servidor: `cd backend`
+2. Instala las dependencias: `npm install`
+4. Inicia el servidor: `npm start`
+
+### Configurar y arrancar el cliente
+
+1. Navega al directorio del cliente: `cd frontend`
+2. Instala las dependencias: `npm install`
+3. Navega entre los archivos `./pages/index.jsx`, `./components/episodemodal.jsx` y modifica la url por la de tu backend.
+4. Inicia el cliente: `npm start`
+
+¡Disfruta!
+
+## ✨ Deployado utilizando
+
+Frontend: [Vercel](https://vercel.com)
+Backend: [Render](https://render.com)
+
+## 📬 Contacto
+
+Puedes contactarme mediante mi [portafolio](https://darioalbor.dev.ar).
+
+También puedes contribuir al proyecto sumando nuevas ideas o líneas de código. ¡Toda ayuda es bienvenida!
+
+## 📸 Imágenes
+
+| ![Inicio](./imagesgit/1.png) | ![Selección de episodio](./imagesgit/2.png) |
+|:------------------------------------------:|:----------------------------------------------------:|
+| Inicio                         | Selección de episodio                                |
+
+| ![Episodio](./imagesgit/3.png)        | ![]()                 |
+|:------------------------------------------:|:----------------------------------------------------:|
+| Vista previa episodio                                    |                                            |
